487773e27cd2275e7c41349ccc49740d3b1505d3
Marcos <mmarcos787@gmail.com>
2022-03-25 17:59:53 -0300

    Algumas modificações

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ const INPUT_AREA = document.querySelector("#input_area");
 const ELEMENTS = [BTN_NEW, BTN_SAVE, BTN_CANCEL, INPUT_AREA];
 
 const elementsList = () => {
-  let notes = document.querySelectorAll(".note-item");
+  let btnResize = document.querySelectorAll(".btn-resize");
   let btnDelete = document.querySelectorAll(".btn-delete");
   let btnEdit = document.querySelectorAll(".btn-edit");
 
-  return [notes, btnDelete, btnEdit];
+  return [btnResize, btnDelete, btnEdit];
 };
 
 // Toggles the visibility of elements
@@ -42,29 +42,34 @@ const createNoteItem = () => {
 
 // Creates and wraps the Delete and Edit buttons
 const createBtns = () => {
-  const BTN_WRAPPER = document.createElement("div");
-  const BTN_DELETE = document.createElement("button");
+  const BTNS_WRAPPER = document.createElement("div");
+  const BTN_RESIZE = document.createElement("button");
   const BTN_EDIT = document.createElement("button");
+  const BTN_DELETE = document.createElement("button");
 
-  BTN_WRAPPER.setAttribute("class", "edit-delete-wrapper");
-  BTN_DELETE.setAttribute("class", "btn-delete material-icons-round");
-  BTN_DELETE.setAttribute("title", "Double click to delete note");
-  BTN_DELETE.innerHTML = "delete_forever";
+  BTNS_WRAPPER.setAttribute("class", "btns-wrapper");
+
+  BTN_RESIZE.setAttribute("class", "btn-resize material-icons-round");
+  BTN_RESIZE.setAttribute("title", "Click to resize note");
+  BTN_RESIZE.innerHTML = "expand_more";
 
   BTN_EDIT.setAttribute("class", "btn-edit material-icons-round");
   BTN_EDIT.setAttribute("title", "Click to edit note");
   BTN_EDIT.innerHTML = "edit";
 
-  BTN_WRAPPER.append(BTN_DELETE, BTN_EDIT);
-  return BTN_WRAPPER;
+  BTN_DELETE.setAttribute("class", "btn-delete material-icons-round");
+  BTN_DELETE.setAttribute("title", "Double click to delete note");
+  BTN_DELETE.innerHTML = "delete_forever";
+
+  BTNS_WRAPPER.append(BTN_RESIZE, BTN_EDIT, BTN_DELETE);
+  return BTNS_WRAPPER;
 };
 
 // Saves a Note
 const saveNote = () => {
   UL.prepend(createNoteContainer());
-
   elementsList();
-  invokeFuncs();
+  initFunctions();
 };
 
 // Buttons actions
@@ -77,8 +82,7 @@ BTN_CANCEL.onclick = () => {
 };
 
 BTN_SAVE.onclick = () => {
-  INPUT_AREA.value.trim() === "" ? false : saveNote();
-  toggleVisibility();
+  INPUT_AREA.value.trim() === "" ? false : saveNote(), toggleVisibility();
   INPUT_AREA.style.height = "auto";
 };
 
@@ -93,54 +97,45 @@ INPUT_AREA.addEventListener(
 );
 
 // Increases and decreases the size of Notes on click
-const expandNote = () => {
-  return elementsList()
-    .at(0)
-    .forEach((element) => {
-      element.onclick = () => {
-        if (element.offsetHeight !== element.scrollHeight) {
-          element.style.height = element.scrollHeight + "px";
-        } else {
-          element.style.height = "45px";
-        }
-      };
-    });
+const resizeNote = (btn_resize) => {
+  btn_resize.forEach((element) => {
+    let note = element.parentNode.previousElementSibling;
+    element.onclick = () => {
+      note.offsetHeight !== note.scrollHeight
+        ? ((note.style.height = note.scrollHeight + "px"),
+          (element.innerHTML = "expand_less"))
+        : ((note.style.height = "45px"), (element.innerHTML = "expand_more"));
+    };
+  });
 };
 
 // Deletes a note by double-clicking the delete button
-const deleteNote = () => {
-  return elementsList()
-    .at(1)
-    .forEach((element) => {
-      element.ondblclick = () => {
-        element.parentNode.parentNode.remove();
-      };
-    });
+const deleteNote = (btn_delete) => {
+  btn_delete.forEach((element) => {
+    let note = element.parentNode.parentNode;
+    element.ondblclick = () => {
+      note.remove();
+    };
+  });
 };
 
 // Not ready yet
-const editNote = () => {
-  return elementsList()
-    .at(2)
-    .forEach((element) => {
-      const DELETE_BTN = element.previousElementSibling;
-      const NOTE = element.parentNode.previousElementSibling;
-      element.onclick = () => {
-        element.innerHTML === "edit"
-          ? ((element.innerHTML = "task_alt"),
-            (DELETE_BTN.style.display = "none"),
-            (NOTE.readOnly = false))
-          : ((element.innerHTML = "edit"),
-            (DELETE_BTN.style.display = ""),
-            (NOTE.readOnly = true));
-      };
-    });
+const editNote = (btn_edit) => {
+  btn_edit.forEach((element) => {
+    let note = element.parentNode.previousElementSibling;
+    element.onclick = () => {
+      element.innerHTML === "edit"
+        ? ((element.innerHTML = "task_alt"),
+          ((note.readOnly = false), note.focus()))
+        : ((element.innerHTML = "edit"), (note.readOnly = true));
+    };
+  });
 };
 
-// Invokes action functions
-const invokeFuncs = () => {
-  expandNote();
-  deleteNote();
-  editNote();
+// Initiate action functions
+const initFunctions = () => {
+  resizeNote(elementsList().at(0));
+  deleteNote(elementsList().at(1));
+  editNote(elementsList().at(2));
 };
-invokeFuncs();
+initFunctions();
